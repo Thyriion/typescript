7d5f4bc0d89ee59da7b1d9517b951c462dd4670a
Thyrrion <kevinschueler2000@gmail.com>
2022-10-17 10:47:47 +0200

    init project

---
+++ src/datatypes.ts
@@ -0,0 +1,5 @@
+const nullValue = null; //kein Wert - meiner
+const undefinedValue = undefined; //kein Wert - gehört der Engine
+
+const x: void; // Kein Rückgabewert;
+const y: never; //Kein Rückgabewert -- wirft immer fehler 
